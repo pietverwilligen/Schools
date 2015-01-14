91d541e2d8267a320320c6f4c3a81058e90c83da
Piet <piet.verwilligen@cern.ch>
2015-01-14 11:23:44 +0000

    work ongoing

--- CMSDAS_2015_Bari/CMSDAS/Muons/run/studyKin.py
+++ CMSDAS_2015_Bari/CMSDAS/Muons/run/studyKin.py
@@ -15,24 +15,22 @@ def looseID(muon,vertex):
 def tightID(muon,vertex):
     return muon.isTightMuon(vertex)
 
-# def detIso(muon,vertex):
-#     return (muon.isolationR03().sumPt+muon.isolationR03().emEt+muon.isolationR03().hadEt)/muon.pt()
+def detIso(muon,vertex):
+    return (muon.isolationR03().sumPt+muon.isolationR03().emEt+muon.isolationR03().hadEt)/muon.pt()<0.25
 
-analyzer.addSelection('tightMuonSelection',tightID,22,ROOT.kBlack)
+# analyzer.addSelection('tightMuonSelection',tightID,22,ROOT.kBlack)
+analyzer.addSelection('DetectorIsolation',detIso,22,ROOT.kBlack)
 
 
 
 # declare plots here like this:
-signDiMuMassBeforeCut = ROOT.TH1F("signDiMuMassBeforeCut","",120, 0,120)
-signDiMuMassAfterCut  = ROOT.TH1F("signDiMuMassAfterCut", "",120, 0,120)
-backDiMuMassBeforeCut = ROOT.TH1F("backDiMuMassBeforeCut","",120, 0,120)
-backDiMuMassAfterCut  = ROOT.TH1F("backDiMuMassAfterCut", "",120, 0,120)
+signMuPtBeforeCut = ROOT.TH1F("signMuPtBeforeCut","",100, 0,100)
+backMuPtBeforeCut = ROOT.TH1F("backMuPtBeforeCut","",100, 0,100)
 
-signPtBeforeCut = ROOT.TH1F("signMuPtBeforeCut","",120, 0,120)
-signPtAfterCut  = ROOT.TH1F("signMuPtAfterCut", "",120, 0,120)
-backPtBeforeCut = ROOT.TH1F("backMuPtBeforeCut","",120, 0,120)
-backPtAfterCut  = ROOT.TH1F("backMuPtAfterCut", "",120, 0,120)
-
+signMuPtAfterCut  = ROOT.TH1F("signMuPtAfterCut", "",100, 0,100)
+backMuPtAfterCut  = ROOT.TH1F("backMuPtAfterCut", "",100, 0,100)
+signMuPtEffCut    = ROOT.TH1F("signMuPtEffCut"  , "",100, 0,100)
+backMuPtEffCut    = ROOT.TH1F("backMuPtEffCut"  , "",100, 0,100)
 
 
 
@@ -40,20 +38,16 @@ backPtAfterCut  = ROOT.TH1F("backMuPtAfterCut", "",120, 0,120)
 # By writing your analysis code here it will be plugged into the analyzer
 # at the appropriate place 
 def fillSignalPlotsBeforeCut(mu2Pt, mu2Eta, mu2Phi, mu2Charge, invMass):
-    signDiMuMassBeforeCut.Fill(invMass)
-    signPtBeforeCut.Fill(mu2Pt)
+    signMuPtBeforeCut.Fill(mu2Pt)
 
 def fillSignalPlotsAfterCut(mu2Pt, mu2Eta, mu2Phi, mu2Charge, invMass):
-    signDiMuMassAfterCut.Fill(invMass)
-    signPtAfterCut.Fill(mu2Pt)
+    signMuPtAfterCut.Fill(mu2Pt)
 
 def fillBackgroundPlotsBeforeCut(mu2Pt, mu2Eta, mu2Phi, mu2Charge, invMass):
-    backDiMuMassBeforeCut.Fill(invMass)
-    backPtBeforeCut.Fill(mu2Pt)
+    backMuPtBeforeCut.Fill(mu2Pt)
 
 def fillBackgroundPlotsAfterCut(mu2Pt, mu2Eta, mu2Phi, mu2Charge, invMass):
-    backDiMuMassAfterCut.Fill(invMass)
-    backPtAfterCut.Fill(mu2Pt)
+    backMuPtAfterCut.Fill(mu2Pt)
 
 
 # assign the void functions in the analyzer with the functions you created here above
@@ -66,25 +60,6 @@ analyzer.backPlotAfterCut  = fillBackgroundPlotsAfterCut
 analyzer.run()
 
 # make a nice plot
-
-signDiMuMassBeforeCut.SetMarkerStyle(20)
-signDiMuMassBeforeCut.SetMarkerColor(ROOT.kRed)  
-signDiMuMassAfterCut.SetMarkerStyle(24)
-signDiMuMassAfterCut.SetMarkerColor(ROOT.kRed)  
-backDiMuMassBeforeCut.SetMarkerStyle(20)
-backDiMuMassBeforeCut.SetMarkerColor(ROOT.kBlue)  
-backDiMuMassAfterCut.SetMarkerStyle(24)
-backDiMuMassAfterCut.SetMarkerColor(ROOT.kBlue)  
-
-c = ROOT.TCanvas("c","c",800,600)
-c.Divide(1,2)
-c.cd(1)
-signDiMuMassBeforeCut.Draw("P")
-signDiMuMassAfterCut.Draw("Psame")
-backDiMuMassBeforeCut.Draw("Psame")
-backDiMuMassAfterCut.Draw("Psame")
-c.Update()
-
 signMuPtBeforeCut.SetMarkerStyle(20)
 signMuPtBeforeCut.SetMarkerColor(ROOT.kRed)  
 signMuPtAfterCut.SetMarkerStyle(24)
@@ -94,10 +69,23 @@ backMuPtBeforeCut.SetMarkerColor(ROOT.kBlue)
 backMuPtAfterCut.SetMarkerStyle(24)
 backMuPtAfterCut.SetMarkerColor(ROOT.kBlue)  
 
+signMuPtEffCut.Divide(signMuPtAfterCut,signMuPtBeforeCut,1,1,"")
+backMuPtEffCut.Divide(backMuPtAfterCut,backMuPtBeforeCut,1,1,"")
+signMuPtEffCut.SetMarkerStyle(28)
+signMuPtEffCut.SetMarkerColor(ROOT.kRed)  
+backMuPtEffCut.SetMarkerStyle(28)
+backMuPtEffCut.SetMarkerColor(ROOT.kBlue)  
 
-c.cd(2)
+
+c = ROOT.TCanvas("c","c",800,600)
+c.Divide(2,1)
+c.cd(1)
 signMuPtBeforeCut.Draw("P")
 signMuPtAfterCut.Draw("Psame")
 backMuPtBeforeCut.Draw("Psame")
 backMuPtAfterCut.Draw("Psame")
+c.Update()
+c.cd(2)
+signMuPtEffCut.Draw("P")
+backMuPtEffCut.Draw("Psame")
 c.Update()
